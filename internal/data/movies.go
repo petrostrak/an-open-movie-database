443c2c38cf059aa3849e16d9259cf468328c0081
petrostrak <pit.trak@gmail.com>
2021-10-20 21:24:33 +0300

    Changing keys in the JSON object

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,11 +3,11 @@ package data
 import "time"
 
 type Movie struct {
-	ID        int64     // Unique integer ID for the movie
-	CreatedAt time.Time // Timestamp for when the movie is added to our  DB
-	Title     string    // Movie title
-	Year      int32     // Movie release year
-	Runtime   int32     // Movie runtime(in minutes)
-	Genres    []string  // Slice of genres for the movie (romance, comedy etc.)
-	Version   int32     // The version number starts at 1 and will be incremented each time the movie info is updated
+	ID        int64     `json:"id"`        // Unique integer ID for the movie
+	CreatedAt time.Time `json:"create_at"` // Timestamp for when the movie is added to our  DB
+	Title     string    `json:"title"`     // Movie title
+	Year      int32     `json:"year"`      // Movie release year
+	Runtime   int32     `json:"runtime"`   // Movie runtime(in minutes)
+	Genres    []string  `json:"genres"`    // Slice of genres for the movie (romance, comedy etc.)
+	Version   int32     `json:"version"`   // The version number starts at 1 and will be incremented each time the movie info is updated
 }
