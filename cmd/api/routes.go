18bfab40b4ff97076ae230a2a48da516c61d300e
petrostrak <pit.trak@gmail.com>
2021-10-21 10:51:48 +0300

    Routing errors

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,9 +6,25 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+var (
+	router *httprouter.Router
+)
+
+func init() {
 	// Initialize a new httprouter router instance.
-	router := httprouter.New()
+	router = httprouter.New()
+}
+
+func (app *application) routes() *httprouter.Router {
+
+	// Convert the notFoundResponse() helper to a http.Handler using the
+	// http.HandlerFunc() adapter, and then set it as the custom error
+	// handler for 404 Not Found responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	// Convert the methodNotAllowedResponse() helper to a http.Handler and see
+	// it as the custom error handler  for 405 Method Not Allowed responses.
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	// Register the relevant methods, URL patterns and handler functions for our
 	// endpoints using the HandlerFunc() method. Note that http.MethodGet and
