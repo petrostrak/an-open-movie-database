79b37f64ffb876fdeb1290b4449ef9b803b5b1fe
petrostrak <pit.trak@gmail.com>
2021-10-21 10:31:01 +0300

    Sending Error Messages

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -19,8 +19,8 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	if err := app.writeJSON(w, http.StatusOK, env, nil); err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		// Use the serverErrorResponse() helper func.
+		app.serverErrorResponse(w, r, err)
 	}
 
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -20,7 +20,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -40,8 +40,8 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	//
 	// Create an envelope{"movie":movie} instance and pass it to writeJSON()
 	if err := app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil); err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		// Use the new serverErrorResponse() helper.
+		app.serverErrorResponse(w, r, err)
 	}
 
 }
