52d55c1aeed65d893a658aae39f92977d5956bc0
petrostrak <pit.trak@gmail.com>
2021-10-21 21:18:53 +0300

    Restricting Inputs with DisallowUnknownFields

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -70,8 +71,21 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
-	// Decode the request body into the target destination.
-	if err := json.NewDecoder(r.Body).Decode(dst); err != nil {
+	// Use http.MaxBytesReader() to limit the size of the request body  to 1MB
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	// Initialize the json.Decoder and call the DisallowUnknownFields() method on it
+	// before decoding. This means that if the JSON from the client now includes any
+	// field which cannot be mapped to the target destination, the decoder will return
+	// an error instead of just ignoring the field.
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	// Decode the request body to the destination.
+	err := dec.Decode(dst)
+
+	if err != nil {
 		// If there is an error during decoding, start the triage
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -93,15 +107,32 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 		// An io.EOF error will be returned by Decode() if the request body is empty.
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
-			// A json.InvalidUnmarshalError error will be returned if we pass a non-nil
-			// pointer to Decode(). We catch this and panic, rather than returning an error
-			// to our handler.
+		// If the JSON contains a field which cannot be mapped to the target destination
+		// then Decode() will now return an error message.
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		// If the request body exceeds 1MB in size the decode will now fail with the
+		// error "http: request body too large".
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+		// A json.InvalidUnmarshalError error will be returned if we pass a non-nil
+		// pointer to Decode(). We catch this and panic, rather than returning an error
+		// to our handler.
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
 			// For anything else, return the error message as-is.
 		default:
 			return err
 		}
+	}
+
+	// Call Decode() again, using a pointer to an empty anonymous struct as the
+	// destination. If the request body only contained a single JSON value this will
+	// return an io.EOF error. So if we get anything else, we know that there is
+	// additional data in the request body and we return our custom error message.
+	if err := dec.Decode(&struct{}{}); err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
 	}
 
 	return nil
