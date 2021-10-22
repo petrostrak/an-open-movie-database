f1cf505c0b7cc3a255963a1df4a640252becf27b
petrostrak <pit.trak@gmail.com>
2021-10-22 20:39:04 +0300

    Making validation rules reusable

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"github.com/petrostrak/an-open-movie-database/internal/validator"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"`                // Unique integer ID for the movie
@@ -10,4 +14,24 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"` // Movie runtime(in minutes)
 	Genres    []string  `json:"genres,omitempty"`  // Slice of genres for the movie (romance, comedy etc.)
 	Version   int32     `json:"version"`           // The version number starts at 1 and will be incremented each time the movie info is updated
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	// Use the Check() method to execute our validation checks. This will add the
+	// provided key and error message to the errors map if the check does not evaluate
+	// to true.
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more that 500 bytes long")
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must bot contain more that 5 genres")
+	// Note that we're using the Unique helper below to check that all values in the
+	// input.Genres slice are unique.
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+
 }
