5889c29a22ffd8f90e2807fa2922d2870e2f56c3
petrostrak <pit.trak@gmail.com>
2021-10-22 21:19:04 +0300

    Establishing a connection postgres pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const (
@@ -14,9 +18,15 @@ const (
 )
 
 // Define a config struct to hold all the configuration settings for our application.
+//
+// Add a db struct field to hold the configuration setting for our database connection
+// pool. For now this only holds the DSN, which we will read in from a command-line flag.
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 // Define an application struct to hold the dependencies for our HTTP handlers, helpers,
@@ -37,9 +47,30 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
 
+	// Read the DSN value from the db-dsn command-line flag into the config struct.
+	// We default to using our development DSN if no flag is provided.
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://omdb:pa55word@localhost/omdb", "PostgreSQL DSN")
+	flag.Parse()
+
 	// Initialize a new logger which writes messages to the standard out stream,
 	// prefixed with the current date and time.
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// Call the openDB() helper function to create the connection pool,
+	// passing in the config struct. If this returns an error, we log it and exit the
+	// application.
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// Defer a call to db.Close() so that the connection pool is closed before the
+	// main() function exits.
+	defer db.Close()
+
+	// Also log a message to say that the connection pool has been successfully
+	// established.
+	logger.Printf("database connection pool established\n")
 
 	// Declare an instance of the application struct, containing the config struct and
 	// the logger.
@@ -66,6 +97,30 @@ func main() {
 
 	// Start the HTTP server.
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+// The openDB() function returns a sql.DB connection pool.
+func openDB(cfg config) (*sql.DB, error) {
+	// Use sql.Open() to create an empty connection pool, using the DSN from the config
+	// struct.
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a context with a 5-second timeout deadline.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Use PingContext() to establish a new connection to the database, passing in the
+	// context we created above as a parameter. If the connection couldn't be
+	// established successfully within the 5 second deadline, then this will return an// error.
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	// Return the sql.DB connection pool
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/petrostrak/an-open-movie-database
 go 1.17
 
 require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/lib/pq v1.10.0 // indirect
