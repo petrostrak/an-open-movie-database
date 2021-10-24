f5c464e0ddd77c9c78616d397f5a0837ccbfb6c9
petrostrak <pit.trak@gmail.com>
2021-10-24 20:26:55 +0300

    Creating the Update API handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -106,6 +106,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		default:
 			app.serverErrorResponse(w, r, err)
 		}
+		return
 	}
 
 	// Encode the struct to JSON and send it as the HTTP response.
@@ -116,4 +117,67 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		app.serverErrorResponse(w, r, err)
 	}
 
+}
+
+func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the movie ID from the URL
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	// Fetch the existing movie record from the DB, sending a 404 NotFound
+	// response to the client if we couln't find a matching record.
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Declare an input struct to hold the expected data from the client.
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	// Read the JSON request body data into the input struct.
+	if err = app.readJSON(w, r, &input); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the request body to the appropriate fields of the movie
+	// record.
+	movie.Title = input.Title
+	movie.Year = input.Year
+	movie.Runtime = input.Runtime
+	movie.Genres = input.Genres
+
+	// Validate the updated movie record, sending the client a 422 Unprocessable Entity
+	// response in any checks fail.
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Pass the unpdated movie record to our new Update() method.
+	if err = app.models.Movies.Update(movie); err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// Write the update movie record in a JSON response.
+	if err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil); err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
