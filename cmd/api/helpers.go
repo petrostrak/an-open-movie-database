8751be3cfe7f2cdfa67732c0138a3ca50e3ee4f7
petrostrak <pit.trak@gmail.com>
2021-10-26 13:26:55 +0300

    Filtering, Sorting, and Pagination - Creating helper functions - readString

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
@@ -136,4 +137,20 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 	}
 
 	return nil
+}
+
+// The readString() helper returns a string value from the query string, or the provided
+// default value if no matching key coyld be found.
+func (app *application) readString(qs url.Values, key, defaultValue string) string {
+	// Extract the value for a given key from the query string. If no key exists, this
+	// will return the empty string "".
+	s := qs.Get(key)
+
+	// If no key exists (or the value is empty) then return the default value.
+	if s == "" {
+		return defaultValue
+	}
+
+	// Otherwise return the string
+	return s
 }
