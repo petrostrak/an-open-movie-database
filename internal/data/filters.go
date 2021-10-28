3ad6a9c4aaae1eb13ba767bfa4d2e5d9b9c4dfbf
petrostrak <pit.trak@gmail.com>
2021-10-28 12:04:05 +0300

    Paginating Lists - Add limit and offset methods

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -30,7 +30,7 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 // Check that the client-provided Sort field matches on of the entries in our safelist
 // and if it does, extract the column name from the Sort field by stripping the leading
 // hyphen character (if one exists).
-func (f *Filters) sortColumn() string {
+func (f Filters) sortColumn() string {
 	for _, safeValue := range f.SortSafelist {
 		if f.Sort == safeValue {
 			return strings.TrimPrefix(f.Sort, "-")
@@ -42,10 +42,25 @@ func (f *Filters) sortColumn() string {
 
 // Return the sort direction ("ASC" or "DESC") depending on the prefix character of the
 // Sort field.
-func (f *Filters) sortDirection() string {
+func (f Filters) sortDirection() string {
 	if strings.HasPrefix(f.Sort, "-") {
 		return "DESC"
 	}
 
 	return "ASC"
+}
+
+// Helper method that returns the page size
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+// Helper method that returns the offset (specific number of rows to be skipped before
+// starting to return records from the query.)
+//
+// There is the theoretical risk of an integer overflow as we are multiplying two int values
+// together. However, this is mitigated by the validation rules we created in our ValidateFilters()
+// function, where we enforced maximum values of page_size=100 and page=10000000
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
 }
