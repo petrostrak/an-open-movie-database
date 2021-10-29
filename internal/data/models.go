70d87075f929fa8c6ee7fac60f71141063875620
petrostrak <pit.trak@gmail.com>
2021-10-29 17:53:01 +0300

    Include the new UserModel in our parent Models struct

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,15 +12,17 @@ var (
 	ErrEditConflict   = errors.New("edit conflict")
 )
 
-// Create a Models struct which wraps the MovieModel.
+// Create a Models struct which wraps the MovieModel and the UserModel.
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 }
 
 // For ease of use, we also add a New() method which returns a Models struct containing
-// the initialized MovieModel.
+// the initialized MovieModel and UserModel.
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
