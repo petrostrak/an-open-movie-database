7ece99f1320db28de63972f07a29105b72eeb84e
petrostrak <pit.trak@gmail.com>
2021-10-31 21:19:58 +0200

    Add DeleteAllForUser method to TokenModel

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -81,7 +81,7 @@ type TokenModel struct {
 }
 
 // Insert() adds the data for a specific token to the tokens table.
-func (m *TokenModel) Insert(token *Token) error {
+func (m TokenModel) Insert(token *Token) error {
 	query := `
 		INSERT INTO tokens (hash, user_id, expiry, scope)
 		VALUES ($1, $2, $3, $4)`
@@ -92,5 +92,19 @@ func (m *TokenModel) Insert(token *Token) error {
 	defer cancel()
 
 	_, err := m.DB.ExecContext(ctx, query, args...)
+	return err
+}
+
+// DeleteAllForUser() delets all tokens for a specific user and scope.
+func (m TokenModel) DeleteAllForUser(scope string, userID int64) error {
+	query := `
+		DELETE FROM tokens
+		WHERE scope = $1 AND user_id = $2`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, scope, userID)
+
 	return err
 }
