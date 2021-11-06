7516adbb365eb8e38457ea326035f96e8efbdcf4
petrostrak <pit.trak@gmail.com>
2021-11-06 10:59:04 +0200

    Update enableCORS to intercept and respond to preflight requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -273,6 +273,9 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 		// Add the "Vary: Origin" header.
 		w.Header().Add("Vary", "Origin")
 
+		// Add the "Vary: Access-Control-Request-Method" header.
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
 		// Get tge value of the request's Origin header.
 		origin := r.Header.Get("Origin")
 
@@ -286,6 +289,20 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 					// If there is a match, then set a "Access-Control-Allow-Origin"
 					// response header with the request origin as the value.
 					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					// Check if the request has the HTTP method OPTIONS and contains the
+					// "Access-Control-Request-Method" header. If it does, then we treat
+					// it as a preflight request.
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						// Set the necessary preflight response headers
+						w.Header().Set("Access-Control-Request-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						// Write the headers along with a 200 status ok and return from
+						// the middleware with no further actions.
+						w.WriteHeader(http.StatusOK)
+						return
+					}
 				}
 			}
 		}
